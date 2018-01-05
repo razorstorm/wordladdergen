56da405c36d1e2181f6b2f17d598c8bf2427ca2d
jason <jason@blend.com>
2018-01-05 15:46:01 -0800

    fix

--- word_ladder.py
+++ word_ladder.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+with open('words.txt', 'r') as words_fh:
+    for word in words_fh:
+        words.append(word.strip())
+
+words_set = set(words)
+
 
 @app.route('/<input_word>', methods=['GET'])
 def serve(input_word):
@@ -20,33 +26,24 @@ def gen_words(input_word):
 
     print('starting')
 
-    with open('words.txt', 'r') as words_fh:
-        for word in words_fh:
-            words.append(word.strip())
-
-    words_set = set(words)
-
     for i, letter in enumerate(input_word):
-        print(potential_words)
         if i == 0:
             # prepend
             for l in letters:
                 potential_word = l + input_word
-                if potential_word in words_set:
+                if potential_word in words_set and potential_word != input_word:
                     potential_words.append(potential_word)
 
         # change
         for l in letters:
             potential_word = input_word[0:i] + l + input_word[i+1:]
-            # print potential_word
-            if potential_word in words_set:
+            if potential_word in words_set and potential_word != input_word:
                 potential_words.append(potential_word)
 
         # append
         for l in letters:
             potential_word = input_word[0:i] + letter + l + input_word[i+1:]
-            # print potential_word
-            if potential_word in words_set:
+            if potential_word in words_set and potential_word != input_word:
                 potential_words.append(potential_word)
 
     return potential_words
