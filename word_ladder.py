becc6990dd50cef700c941d297281639299bd653
jason <jason@blend.com>
2018-01-05 14:44:55 -0800

    set up flask

--- word_ladder.py
+++ word_ladder.py
@@ -1,33 +1,52 @@
+# coding=utf-8
+import json
+import random
+
+import markovify
+import numpy as np
+
+import requests
+from flask import request, Flask
+
+app = Flask(__name__)
+
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-words = []
 
-with open('/usr/share/dict/words', 'r') as words_fh:
-    for word in words_fh:
-        words.append(word.strip())
 
-words_set = set(words)
+@app.route('/<input_word>', methods=['GET'])
+def serve(input_word):
+    return gen_words(input_word)
 
-input_word = 'hat'
 
-for i, letter in enumerate(input_word):
-    if i == 0:
-        # prepend
-        for l in letters:
-            potential_word = l + input_word
-            print potential_word
-            if potential_word in words_set:
+def gen_words(input_word):
+    words = []
+    potential_words = []
+
+    with open('/usr/share/dict/words', 'r') as words_fh:
+        for word in words_fh:
+            words.append(word.strip())
+
+    words_set = set(words)
+
+    for i, letter in enumerate(input_word):
+        if i == 0:
+            # prepend
+            for l in letters:
+                potential_word = l + input_word
                 print potential_word
+                if potential_word in words_set:
+                    potential_words.append(potential_word)
 
-    # change
-    for l in letters:
-        potential_word = input_word[0:i] + l + input_word[i+1:]
-        # print potential_word
-        if potential_word in words_set:
-            print potential_word
+        # change
+        for l in letters:
+            potential_word = input_word[0:i] + l + input_word[i+1:]
+            # print potential_word
+            if potential_word in words_set:
+                potential_words.append(potential_word)
 
-    # append
-    for l in letters:
-        potential_word = input_word[0:i] + letter + l + input_word[i+1:]
-        # print potential_word
-        if potential_word in words_set:
-            print potential_word
+        # append
+        for l in letters:
+            potential_word = input_word[0:i] + letter + l + input_word[i+1:]
+            # print potential_word
+            if potential_word in words_set:
+                potential_words.append(potential_word)
